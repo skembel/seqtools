418c024d679f3a35fa419ea59627c26fc509f5a3
Steven Kembel <steve.kembel@gmail.com>
2017-08-04 18:45:24 -0400

    Cleanup unused files, prepare to use roxygen2 documentation, add first function

---
+++ R/taxocomm.R
@@ -0,0 +1,13 @@
+#' Create taxonomy-aggregated community data.frame
+#'
+#' @param comm community format data.frame (samples in rows, taxa in columns, rows and columns have informative names)
+#' @param taxo a data frame with rows in same order as columns of \code{comm} and taxonomic information for each row provided in columns
+#' @param rank Taxonomic rank to aggregate at (a column name in \code{taxo})
+#' @return A community data.frame where columns are taxonomic groups
+#' @export
+taxocomm <- function(comm, taxo, rank) {
+  comm.taxo <- aggregate(t(comm), by=list(taxonrank=taxo[,rank]), sum)
+  comm.taxo <- comm.taxo[-1,]
+  rownames(comm.taxo) <- comm.taxo[,1]
+  return(t(comm.taxo[,-1]))
+}
