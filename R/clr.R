708a1df66a282473d4f4873e6f6535c00371d204
Steve Kembel <steve.kembel@gmail.com>
2020-10-19 14:21:40 -0400

    Update CLR calculations with option to include/exclude zeroes

--- R/clr.R
+++ R/clr.R
@@ -1,19 +1,21 @@
-gm_mean = function(x, na.rm=TRUE){
-  exp(sum(log(x[x > 0 & !is.na(x)]), na.rm=na.rm) / length(x))
-}
-
-clr = function(x, base=2){
-  x <- log((x / gm_mean(x)), base)
-  x[!is.finite(x) | is.na(x)] <- 0.0
-  return(x)
+gm_mean = function(x, ignore.zeroes=TRUE, na.rm=TRUE){
+  if (ignore.zeroes) {
+    exp(sum(log(x[x > 0 & !is.na(x)]), na.rm=na.rm) / length(x))
+  } else {
+    exp(sum(log(x[!is.na(x)]), na.rm=na.rm) / length(x))
+  }
 }
 
 #' Centered log-ratio (CLR) transform community data
 #'
 #' @param x A vegan-format matrix of samples by taxa
+#' @param base Base for logarithm (default=2)
+#' @param ignore.zeroes Should zeros be included when calculating geometric mean (default=TRUE)
 #'
 #' @return CLR-transformed matrix
 #' @export
-clr_transform = function(x) {
+clr_transform = function(x, base=2, ignore.zeroes=TRUE) {
+  x <- log((x / gm_mean(x, ignore.zeroes)), base)
+  x[!is.finite(x) | is.na(x)] <- 0.0
   return(t(apply(x, 1, clr)))
 }
